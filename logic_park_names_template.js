7d3b5396f6f7438bcaf680af56facb7c285c7fc6
hmmclean <mclean.haylee@gmail.com>
2024-01-29 21:25:47 -0500

    Update logic_template to final code

--- logic_park_names_template.js
+++ logic_park_names_template.js
@@ -13,15 +13,6 @@ function createFeatures(parksData) {
   let geojsonFeatures = []
 
   parksData.forEach(function (park) {
-    // Once state code is ready comment back in the below:
-    // // Parse the addresses JSON-like string into a JavaScript object
-    // const addresses = JSON.parse(park.addresses);
-
-    // // Find the stateCode from the first address (assuming it's present in all addresses)
-    // const stateCode = addresses.length > 0 ? addresses[0].stateCode : '';
-    
-    // add in marker color to green
-
     let geojsonFeature = {
       type: "Feature",
       geometry: {
@@ -30,7 +21,7 @@ function createFeatures(parksData) {
       },
       properties: {
         fullName: park.fullName,
-        //stateCode: stateCode,
+        stateCode: park.states,
         description: park.description,
         activities: park.activities,
         url: park.url
@@ -39,21 +30,66 @@ function createFeatures(parksData) {
 
     geojsonFeatures.push(geojsonFeature);
   });
-  
-  // Create a GeoJSON layer with the features array
-  // Add a link to open street maps (pulls in lat and lon) 
-  // Collapsable description?
-  let parks = L.geoJSON(geojsonFeatures, {
-    onEachFeature: function (feature, layer) {
-      layer.bindPopup(`<h3>${feature.properties.fullName}</h3>
-      <hr>
-      <p>${feature.properties.description}</p>
-      <p>${feature.properties.url}<p>
-      `);
-    }
-  });
 
-  // Send our GeoJSON layer to the createMap function
+  // Function to create pointer markers with dark green color.
+  let greenIcon = L.divIcon({
+      className: 'custom-icon',
+      html: '<div class="icon-wrapper"><i class="fas fa-map-marker" style="color: darkgreen; font-size: 2em"></i><div class="circle"><div><div>',
+      iconSize: [48, 64],
+      iconAnchor: [24, 50],
+      popupAnchor: [0, -42]
+    });
+  
+  // Create a GeoJSON layer with the features array.
+  let parks = L.geoJSON(geojsonFeatures, {
+    pointToLayer: function (feature, latlng) {
+      // use the default marker but set to green.
+      return L.marker(latlng, {icon: greenIcon});
+    },
+
+    onEachFeature: function (feature, layer) {
+       // Truncate description to a certain length.
+      const maxDescriptionLength = 100;
+      const truncatedDescription = feature.properties.description.length > maxDescriptionLength
+        ? feature.properties.description.substring(0, maxDescriptionLength) + "..."
+        : feature.properties.description;
+
+        layer.bindPopup(`<h3>${feature.properties.fullName}, ${feature.properties.stateCode}</h3>
+        <hr>
+        <p id="truncatedDescription">${truncatedDescription}<a href="#" class="read-more">Read more</a></p>
+        <hr>
+        <div id="fullDescription" style="display:none;">${feature.properties.description}</div>
+        <details>
+          <summary>View Park Website</summary>
+          <p>Website: <a href="${feature.properties.url}" target="_blank">${feature.properties.url}</a></p>
+        </details>
+        <details>
+          <summary>View Park Activities</summary>
+          <p>Activities: ${feature.properties.activities}</p>
+        </details>
+        <details>
+          <summary>Travel to <i>${feature.properties.fullName}</i></summary>
+          <p>Get Directions: <a href="https://www.google.com/maps/dir/?api=1&destination=${feature.geometry.coordinates[1]},${feature.geometry.coordinates[0]}" target="_blank">Google Maps</a></p>
+        </details>
+        `);
+  
+        // Attach click event dynamically after the popup is opened.
+        layer.on('popupopen',function (){
+                  const popupElement = layer.getPopup().getElement();
+                  popupElement.querySelector('.read-more').addEventListener('click', expandDescription);
+        });
+      }
+    });
+  
+    // Function to expand the description.
+    function expandDescription() {
+      const truncatedDescription = document.getElementById("truncatedDescription");
+      const fullDescription = document.getElementById("fullDescription");
+      truncatedDescription.style.display = "none";
+      fullDescription.style.display = "block";
+    }
+
+  // Send our GeoJSON layer to the createMap function.
   createMap(parks);
 }
 
@@ -74,21 +110,7 @@ function createMap(parks) {
     attribution: '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="https://carto.com/attributions">CARTO</a>'
   }).addTo(myMap);
 
-  // Create a baseMaps object.
-  let baseMaps = {
-    "Street Map": tileLayer,
-  };
-
-  // Create an overlay object to hold our overlay.
-  let overlayMaps = {
-    Parks: parks
-  };
-
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
+  // Add the parks GeoJSON layer to the map.
+  parks.addTo(myMap)
 
 }
