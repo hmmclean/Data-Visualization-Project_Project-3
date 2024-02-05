6933ad14b997dc141ee93563e9f1f6cfee4f37a6
Tia Scott <145622440+tiascott01@users.noreply.github.com>
2024-02-05 10:51:17 -0500

    Delete REVISED Park Search directory

--- REVISED Park Search/static/js/popup_logic.js
+++
@@ -1,9 +0,0 @@
-// Function to show the pop-up
-function showPopup() {
-    document.getElementById('popupContainer').style.display = 'block';
-}
-
-// Function to close the pop-up
-function closePopup() {
-    document.getElementById('popupContainer').style.display = 'none';
-}
